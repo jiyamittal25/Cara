5f0d7effbd076e5897febc9cd97bc910617526e8
jiyamittal25 <jiyamittal25@gmail.com>
2023-10-21 01:08:50 +0530

    login file added and code changes

--- JavaScript_files/Homeproducts.js
+++ JavaScript_files/Homeproducts.js
@@ -1,11 +1,10 @@
 // home page
 let p1 = document.getElementById("p1");
-let basket1 = JSON.parse(localStorage.getItem("data")) || [];
 let generateShop1 = () => {
   return (p1.innerHTML = shopItemsData.slice(0,shopItemsData.length/2)
     .map((x) => {
       let { id, name, price, desc, img } = x;
-      let search = basket1.find((x) => String(x.id) === id) || [];
+      let search = basket.find((x) => String(x.id) === id) || [];
       return `<div class="pro">
                 <img src=${img} alt="">
                 <div class="des">
@@ -38,23 +37,22 @@ generateShop1();
 
 let increment1 = (id) => {
   let selectedItem = id;
-  let search = basket1.find((x) => x.id === selectedItem);
+  let search = basket.find((x) => x.id === selectedItem) ;
   if (search === undefined) {
-    basket1.push({
+    basket.push({
       id: selectedItem,
       item: 1,
     });
   } else {
     search.item += 1;
   }
-  // generateShop1();
   update1(selectedItem);
-  localStorage.setItem("data", JSON.stringify(basket1));
+  localStorage.setItem("data", JSON.stringify(basket));
 };
 
 let decrement1 = (id) => {
   let selectedItem = id;
-  let search = basket1.find((x) => x.id === selectedItem);
+  let search = basket.find((x) => x.id === selectedItem);
 
   if (search === undefined) return;
   else if (search.item === 0) return;
@@ -62,33 +60,32 @@ let decrement1 = (id) => {
     search.item -= 1;
   }
   update1(selectedItem);
-  basket1 = basket1.filter((x) => x.item !== 0);
-  // generateShop1();
-  localStorage.setItem("data", JSON.stringify(basket1));
+  basket = basket.filter((x) => x.item !== 0);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
 
 let update1 = (id) => {
   let selectedItem = id;
-  let search = basket1.find((x) => x.id === selectedItem);
+  let search = basket.find((x) => x.id === selectedItem);
   document.getElementById(id).innerHTML = search.item;
   calculation1();
 };
 
 let calculation1 = () => {
   let cartIcon = document.getElementById("cartamount");
-  cartIcon.innerHTML = basket1.map((x) => x.item).reduce((x, y) => x + y, 0);
+  basket1 = JSON.parse(localStorage.getItem("data")) || [];
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
 calculation1();
 
 
 let p2 = document.getElementById("p2");
-let basket2 = JSON.parse(localStorage.getItem("data")) || [];
 let generateShop2 = () => {
   return (p2.innerHTML = shopItemsData
     .slice(shopItemsData.length / 2,shopItemsData.length)
     .map((x) => {
       let { id, name, price, desc, img } = x;
-      let search = basket2.find((x) => String(x.id) === id) || [];
+      let search = basket.find((x) => String(x.id) === id) || [];
       return `<div class="pro">
                 <img src=${img} alt="">
                 <div class="des">
@@ -104,11 +101,11 @@ let generateShop2 = () => {
                     <h4>${price}</h4>
                 </div>
                     <div class="buttons">
-                        <i  onclick="decrement2(${id})" class="fas fa-square-minus"></i>
+                        <i  onclick="decrement1(${id})" class="fas fa-square-minus"></i>
                         <span id=${id} class="quantity">${
         search.item === undefined ? 0 : search.item
       }</span>
-                        <i onclick="increment2(${id})" class="fas fa-square-plus"></i>
+                        <i onclick="increment1(${id})" class="fas fa-square-plus"></i>
                     </div>
             </div>
         `;
@@ -117,96 +114,4 @@ let generateShop2 = () => {
 };
 generateShop2();
 
-let increment2 = (id) => {
-  let selectedItem = id;
-  let search = basket2.find((x) => x.id === selectedItem);
-  if (search === undefined) {
-    basket2.push({
-      id: selectedItem,
-      item: 1,
-    });
-  } else {
-    search.item += 1;
-  }
-  update2(selectedItem);
-  // generateShop2();
-  localStorage.setItem("data", JSON.stringify(basket2));
-  
-};
 
-let decrement2 = (id) => {
-  let selectedItem = id;
-  let search = basket2.find((x) => x.id === selectedItem);
-
-  if (search === undefined) return;
-  else if (search.item === 0) return;
-  else {
-    search.item -= 1;
-  }
-  update2(selectedItem);
-  basket2 = basket2.filter((x) => x.item !== 0);
-  // generateShop2();
-  localStorage.setItem("data", JSON.stringify(basket2));
-  
-};
-
-let update2 = (id) => {
-  let selectedItem = id;
-  let search = basket2.find((x) => x.id === selectedItem);
-  document.getElementById(id).innerHTML = search.item;
-  calculation2();
-};
-
-let calculation2 = () => {
-  let cartIcon = document.getElementById("cartamount");
-  cartIcon.innerHTML = basket2.map((x) => x.item).reduce((x, y) => x + y, 0);
-};
-calculation2();
-
-
-//features
-// let feature = document.getElementById("feature");
-// let feature_data = [
-//   {
-//     id: 1,
-//     img: "../images/f1.png",
-//     desc: "FREE SHIPING",
-//   },
-//   {
-//     id: 2,
-//     img: "../images/f2.png",
-//     desc: "ONLINE ORDER",
-//   },
-//   {
-//     id: 3,
-//     img: "../images/f3.png",
-//     desc: "SAVE MONEY",
-//   },
-//   {
-//     id: 4,
-//     img: "../images/f4.png",
-//     desc: "PROMOTIONS",
-//   },
-//   {
-//     id: 5,
-//     img: "../images/f5.png",
-//     desc: "HAPPY SELLS",
-//   },
-//   {
-//     id: 6,
-//     img: "../images/f6.png",
-//     desc: "F24/7 SUPPORT",
-//   },
-// ];
-// function generatefeatures(){
-//   return (feature.innerHTML=feature_data.map((x)=>{
-//     let {img,desc}=x;
-//     return `
-//         <div class="fe-box">
-//             <img src=${img} alt="">
-//             <h6>${desc}</h6>
-//         </div>
-//     `;
-//   }).join(""));
-// }
-// generatefeatures();
--- JavaScript_files/cart.js
+++ JavaScript_files/cart.js
@@ -124,12 +124,13 @@ totalAmount();
 
 function checkout(){
   let product_placed=document.getElementById("product_placed");
-  product_placed.style.backgroundColor="blue";
+  // product_placed.style.backgroundColor="blue";
   product_placed.style.display= "inline-block";
-  product_placed.style.height= "auto";
-  product_placed.style.padding= "20px";
+  // product_placed.style.height= "auto";
+  // product_placed.style.padding= "20px";
   product_placed.style.textAlign= "center";
-  product_placed.style.borderRadius= "4px";;
+  // product_placed.style.borderRadius= "4px";;
+  product_placed.style.color = "#088178";
   product_placed.innerHTML = "YOUR ORDER HAS BEEN PLACED";
   alert("Your order has been Placed");
   console.log(product_placed);
